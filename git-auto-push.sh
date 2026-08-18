93d6a7117e65bce6a90d77137b1b454c471652b6
Kai-Kodes <studiousguy73@gmail.com>
2026-08-18 09:47:20 +0530

    Auto-update 2026-08-18 09:47:20

---
+++ git-auto-push.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd /home/redsheronin/Files/Coding/College/FSD/CSE-21/JS/ || exit 1
+
+while true; do
+    if [[ -n $(git status --porcelain) ]]; then
+        git add .
+        git commit -m "Auto-update $(date '+%Y-%m-%d %H:%M:%S')"
+        git push origin main
+    fi
+
+    sleep 300
+done
