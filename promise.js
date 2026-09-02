5dd8f542d3e12c99b2f3281e2e79453d1977df3b
Kai-Kodes <studiousguy73@gmail.com>
2026-09-02 14:30:42 +0530

    Auto-update 2026-09-02 14:30:42

--- promise.js
+++ promise.js
@@ -1,7 +1,17 @@
 function f1() {
-    setTimeout(() => {
-        console.log('f1')
-    }, 2000);
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('f1')
+            resolve()
+        }, 4000);
+    })
 }
 
 function f2() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('f2')
+            resolve()
+        }, 1000);
+    })
+}
