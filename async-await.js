2369f43529c5a2b9e8a151fdb0c2b829b06486f2
Kai-Kodes <studiousguy73@gmail.com>
2026-09-08 09:07:30 +0530

    Auto-update 2026-09-08 09:07:30

---
+++ async-await.js
@@ -0,0 +1,22 @@
+function f1() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('f1')
+            resolve()
+        }, 4000);
+    })
+}
+
+function f2() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('f2')
+            resolve()
+        }, 1000);
+    })
+}
+
+async function test(){
+    await f1();
+    await f2();
+}
